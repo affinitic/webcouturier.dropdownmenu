89a24bc968393c9b68761687f0541004fea4e42a
Benoît Suttor <benoit.suttor@imio.be>
2014-12-05 11:23:19 +0100

    Back to development: 2.3.2.cpskinmigration2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.3.2.cpskinmigration1'
+version = '2.3.2.cpskinmigration2.dev0'
 
 setup(name='webcouturier.dropdownmenu',
       version=version,
