b8325ce6ef544bf1e4608229e9b405b348b150cc
Benoît Suttor <benoit.suttor@imio.be>
2015-01-21 15:35:16 +0100

    improve uninstall steps

--- src/webcouturier/dropdownmenu/setuphandlers.py
+++ src/webcouturier/dropdownmenu/setuphandlers.py
@@ -18,3 +18,12 @@ def uninstall(context):
     portal = context.getSite()
     uninstall_configlet(portal)
     remove_properties(portal)
+    installer = getToolByName(portal, 'portal_quickinstaller')
+    if installer.isProductInstalled('webcouturier.dropdownmenu'):
+        installer.uninstallProducts(['webcouturier.dropdownmenu'])
+
+    # delete registred upgrade steps
+    from Products.GenericSetup.interfaces import IUpgradeSteps
+    from zope.component import getGlobalSiteManager
+    sm = getGlobalSiteManager()
+    sm.unregisterUtility(provided=IUpgradeSteps, name=u'webcouturier.dropdownmenu:default')
